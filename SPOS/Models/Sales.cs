564a998944099aa389b1149104676512795772cc
matrook <matrook.cse@outlook.com>
2020-03-09 13:25:13 +0600

    before zohor

---
+++ SPOS/Models/Sales.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace SPOS.Models
+{
+    public class Sales
+    {
+
+        public Sales()
+        {
+            this.T_SALES_DTL = new List<SelectListItem>();
+            this.T_SALES_MST = new List<SelectListItem>();
+            //this.Cities = new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> T_SALES_DTL { get; set; }
+        public List<SelectListItem> T_SALES_MST { get; set; }
+        //public List<SelectListItem> Cities { get; set; }
+
+        public int ReceiveIdDTL { get; set; }
+        public int ReceiveDetailId { get; set; }
+        //public int CityId { get; set; }
+
+        public T_SALES_DTL receiveIdDTL { get; set; }
+        public T_SALES_MST ReceiveDetailIdMST { get; set; }
+
+
+
+
+    }
+}
